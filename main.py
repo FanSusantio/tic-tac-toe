ab261782ea84a0a61758719dc1ff93496417f620
FanSusantio <92912460+FanSusantio@users.noreply.github.com>
2022-08-09 13:57:49 +1000

    Set up todo list

---
+++ main.py
@@ -0,0 +1,32 @@
+############### TicTacToe Project #####################
+
+############### Our TicTacToe Rules #####################
+# The gameplay will be as follows.
+
+# First, one user will place their sign in one of the available empty boxes.
+# Next, the second user will place their sign in one of the available empty boxes.
+# The goal of the players is to place their respective signs completely row-wise or column-wise, or diagonally.
+# The game goes on until a player wins the game or it ended up in a draw by filling all boxes without a winning match.
+
+#TODO-1 Create a board using a 2-dimensional array and initialize each element as empty.
+
+#TODO-2 Write a function to check whether the board is filled or not.
+
+#TODO-3 Write a function to check whether a player has won or not.
+
+#TODO-4 Write a function to show the board as we will show the board multiple times to the users while they are playing.
+
+#TODO-5 Write a function to start the game.
+
+#Select the first turn of the player randomly.
+#Write an infinite loop that breaks when the game is over (either win or draw).
+#Show the board to the user to select the spot for the next move.
+#Ask the user to enter the row and column number.
+#Update the spot with the respective player sign.
+#Check whether the current player won the game or not.
+#If the current player won the game, then print a winning message and break the infinite loop.
+#Next, check whether the board is filled or not.
+#If the board is filled, then print the draw message and break the infinite loop.
+#Finally, show the user the final view of the board.
+
+#Select one or two player game
